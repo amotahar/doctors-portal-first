f6e138c51bf06fc047cf55405618565d3bf14375
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-05-20 18:48:51 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes/Routes";
 
 function App() {
   return (
-    <div className="App">
-      <button className="btn">Button</button>
-      <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
-      <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
+    <div className="max-w-[1440px] mx-auto">
+       <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
